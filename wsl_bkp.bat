ea52ad183350fa828041a7bb22c1d1057dd8af56
Julio Teixeira <jcesarprog@gmail.com>
2022-01-01 04:57:37 -0300

    added wsl ubuntu backup script

---
+++ wsl_bkp.bat
@@ -0,0 +1,18 @@
+@ECHO OFF
+SET turnOff=%1
+
+ECHO Deleting old backup file.
+del "F:\WSL-Backup\Ubuntu20_bkp.tar"
+
+ECHO Stopping WSL machines.
+wsl --shutdown
+
+ECHO Starting WSL Ubuntu backup.
+wsl --export Ubuntu F:\WSL-Backup\Ubuntu20_bkp.tar
+ECHO WSL Ubuntu backup DONE!!.
+
+IF "%turnOff%"=="-d" (
+	ECHO "Shutting down"
+	Stop-Computer
+)
+PAUSE
